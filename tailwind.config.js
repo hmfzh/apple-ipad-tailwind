712e7049b2e9337b837680221542bdd04d206682
ilham <muhammad.hafizha-guest@moladin.com>
2025-02-14 16:04:57 +0700

    feat: Create Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        background: "#FAFAFA"
+      }
+    },
   },
   plugins: [],
 }
