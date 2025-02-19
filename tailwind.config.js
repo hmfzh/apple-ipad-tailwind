2c9c7262b52e51a8d90fbf9e535be54bc1ab26a6
ilham <muhammad.hafizha-guest@moladin.com>
2025-02-19 17:03:10 +0700

    feat : section and frame vidio

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,26 @@ module.exports = {
         roboto: ["Roboto", "sans-serif"],
       },
       colors: {
-        background: "#FAFAFA"
-      }
+        background: "#FAFAFA",
+        red: {
+          apple: "#b64400",
+        },
+        black: {
+          apple: "#1d1d1f",
+        },
+        blue: {
+          apple: "#0066CC",
+        },
+        finish: {
+          "space-black": "#2E2C2E",
+          silver: "#E3E4E5",
+        },
+      },
+      fontSize: {
+        xxs: ["10px", "12px"],
+        heading: ["80px", "84px"],
+        subheading: ["28px", "32px"],
+      },
     },
   },
   plugins: [],
