d46b5a291b7a733a073e26097a01b5502214e0e0
ilham <muhammad.hafizha-guest@moladin.com>
2025-03-13 11:26:50 +0700

    feat: Create slidebar to website

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  }
+
+});
